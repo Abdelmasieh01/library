46efb0a48cc14ce5e1fce2c6f5ec0401395f8e44
Abdelmasieh01 <gokuabdo31@gmail.com>
2023-05-27 21:44:34 +0300

    editing to add keyword variations

--- books/views.py
+++ books/views.py
@@ -8,6 +8,7 @@ from http import HTTPStatus
 from .models import Book, Borrower, Borrowing
 from .forms import BorrowerForm, BorrowingForm, ReturnForm, BookForm, BookUpdateForm
 
+
 def index(request):
     categories = [
         (200, 'منوعات'),
@@ -23,20 +24,30 @@ def index(request):
         (3000, 'المجلدات'),]
     return render(request, 'books/index.html', {'categories': categories})
 
+
 def search(request):
     keyword = request.POST.get('search', '')
-    books = Book.objects.filter(Q(name__icontains=keyword)).order_by('category', 'code')
-    books_author = Book.objects.filter(Q(author__icontains=keyword)).order_by('category', 'code')
-    context = {'books': books, 'books_author': books_author, 'keyword': keyword}
+    keyword_var1 = keyword.replace('أ', 'ا')
+    keyword_var2 = keyword.replace('ى', 'ي')
+    keyword_var3 = keyword_var2.replace('أ', 'ا')
+    books = Book.objects.filter(Q(name__icontains=keyword) | Q(name__icontains=keyword_var1) | Q(
+        name__icontains=keyword_var2) | Q(name__icontains=keyword_var3)).order_by('category', 'code')
+    books_author = Book.objects.filter(Q(author__icontains=keyword) | Q(author__icontains=keyword_var1) | Q(
+        author__icontains=keyword_var1) | Q(author__icontains=keyword_var1)).order_by('category', 'code')
+    context = {'books': books,
+               'books_author': books_author, 'keyword': keyword}
     return render(request, 'books/search.html', context)
+
 
 class BorrowerCreateView(LoginRequiredMixin, CreateView):
     login_url = '/admin/login/'
     model = Borrower
     form_class = BorrowerForm
     template_name = 'books/borrower_form.html'
+
     def get_success_url(self):
         return reverse('books:create-borrower',)
+
 
 @login_required(login_url='/admin/login/')
 def create_borrowing(request):
@@ -45,17 +56,18 @@ def create_borrowing(request):
         if form.is_valid():
             category = form.cleaned_data['category']
             code = form.cleaned_data['code']
-            
+
             try:
                 book = Book.objects.get(category=category, code=code)
             except:
                 error = True
                 return render(request, 'books/borrowing_form.html', {'form': form, 'error': error})
-            
+
             borrower = form.cleaned_data['borrower']
             date = form.cleaned_data['borrow_date']
             if book.copies > 0:
-                borrowing = Borrowing.objects.create(borrower=borrower, borrow_date=date, book=book)
+                borrowing = Borrowing.objects.create(
+                    borrower=borrower, borrow_date=date, book=book)
                 borrowing.borrow()
                 borrower.books.add(book)
                 borrower.save()
@@ -71,12 +83,14 @@ def create_borrowing(request):
     form = BorrowingForm()
     return render(request, 'books/borrowing_form.html', {'form': form, 'error': error})
 
+
 @login_required(login_url='/admin/login/')
 def return_book(request):
     if request.method == 'POST':
         form = ReturnForm(request.POST)
         if form.is_valid():
-            borrowing = Borrowing.objects.get(pk=request.POST.get('borrowing', ''))
+            borrowing = Borrowing.objects.get(
+                pk=request.POST.get('borrowing', ''))
             borrowing.borrower.books.remove(borrowing.book)
             borrowing.return_date = form.cleaned_data['return_date']
             borrowing.returned = True
@@ -85,17 +99,20 @@ def return_book(request):
             borrowing.book.save()
             borrowing.save()
             return redirect('books:return-book')
-    
+
     form = ReturnForm()
     return render(request, 'books/return_book.html', {'form': form})
+
 
 class BookCreateView(LoginRequiredMixin, CreateView):
     login_url = '/admin/login/'
     model = Book
     form_class = BookForm
     template_name = 'books/book_form.html'
+
     def get_success_url(self):
         return reverse('books:create-book',)
+
 
 @login_required(login_url='/admin/login/')
 def edit_book(request):
@@ -109,13 +126,14 @@ def edit_book(request):
             form = False
             error = True
             return render(request, 'books/book_edit.html', {'form': form, 'error': error}, status=HTTPStatus.NOT_FOUND)
-        
+
     form = False
     error = False
     return render(request, 'books/book_edit.html', {'form': form, 'error': error})
 
+
 @login_required(login_url='/admin/login/')
-def edit_book_details(request, pk=2653):      
+def edit_book_details(request, pk=2653):
     if request.method == 'POST':
         form = BookUpdateForm(request.POST)
         if form.is_valid():
@@ -130,10 +148,8 @@ def edit_book_details(request, pk=2653):
             except:
                 error = True
                 return render(request, 'books/book_edit.html', {'form': form, 'error': error}, status=HTTPStatus.NOT_FOUND)
-            
+
     book = Book.objects.get(pk=pk)
     form = BookUpdateForm(instance=book)
     error = False
     return render(request, 'books/book_edit.html', {'form': form, 'error': error})
-
-
