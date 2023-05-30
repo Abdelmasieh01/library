cc80192f4d4e55baacc64e30ddcd3f325bb86867
Abdelmasieh01 <gokuabdo31@gmail.com>
2023-05-30 19:18:23 +0300

    Making two new endpoints, get_post and get_book

--- api/urls.py
+++ api/urls.py
@@ -4,7 +4,9 @@ from . import views
 app_name = 'api'
 
 urlpatterns = [
-    path('books/', views.BookAPIView.as_view(), name='books'),
+    path('books/', views.BookListAPIView.as_view(), name='books'),
     path('posts/', views.PostAPIView.as_view(), name='posts'),
+    path('book/', views.get_book, name='book'),
+    path('post/', views.get_post, name='post'),
     path('profile/', views.get_profile, name='profile'),
 ]
--- api/views.py
+++ api/views.py
@@ -7,23 +7,26 @@ from rest_framework import filters
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework.views import APIView
 from rest_framework.pagination import PageNumberPagination
+
 
 class ZeroIndexedPagePagination(PageNumberPagination):
     page_size = 5
 
     def get_page_number(self, request, paginator):
-        page_number =  super().get_page_number(request, paginator)
+        page_number = super().get_page_number(request, paginator)
         return int(page_number) + 1
 
-class BookAPIView(generics.ListAPIView):
+
+class BookListAPIView(generics.ListAPIView):
     serializer_class = BookSerializer
     pagination_class = ZeroIndexedPagePagination
 
     def get_queryset(self):
         queryset = Book.objects.all().order_by('category', 'code')
         search = self.request.query_params.get('search')
-        if search is not None:
+        if search is not None and search != "":
             search_var1 = search.replace('ا', 'أ')
             search_var2 = search.replace('ي', 'ى')
             search_var3 = search_var2.replace('ا', 'أ')
@@ -36,13 +39,40 @@ class BookAPIView(generics.ListAPIView):
 
         return queryset
 
-
 class PostAPIView(generics.ListAPIView):
     pagination_class = ZeroIndexedPagePagination
     queryset = Post.objects.all().order_by('-timestamp')
     serializer_class = PostSerializer
     filter_backends = [filters.SearchFilter]
     search_fields = ['title', 'profile__name']
+
+
+@api_view(['GEt'])
+def get_book(request):
+    if request.query_params:
+        pk = request.query_params.get('pk')
+        try:
+            book = Book.objects.get(pk=int(pk))
+            item = BookSerializer(book)
+            return Response(item.data, status=status.HTTP_200_OK)
+        except:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+    else:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+
+@api_view(['GET'])
+def get_post(request):
+    if request.query_params:
+        pk = request.query_params.get('pk')
+        try:
+            post = Post.objects.get(pk=int(pk))
+            item = PostSerializer(post)
+            return Response(item.data, status=status.HTTP_200_OK)
+        except:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+    else:
+        return Response(status=status.HTTP_404_NOT_FOUND)
 
 
 @api_view(['GET'])
