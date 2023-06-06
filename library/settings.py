57b398690c2df3b711d3276856d88ffe4707520e
Abdelmasieh01 <gokuabdo31@gmail.com>
2023-06-06 07:20:15 +0300

    Added token auth to rest_framework

--- library/settings.py
+++ library/settings.py
@@ -148,6 +148,9 @@ STORAGES = {
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 15,
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+       'rest_framework.authentication.TokenAuthentication',
+   ),
 }
 
 FIXTURES_DIRS = [
