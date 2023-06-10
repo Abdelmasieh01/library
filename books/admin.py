88ca60c8e8003c2f2d89a76d233e9851c3111288
Abdelmasieh01 <gokuabdo31@gmail.com>
2023-06-10 14:26:54 +0300

    Added recommendation in admin page

--- books/admin.py
+++ books/admin.py
@@ -14,6 +14,10 @@ class BorrowingAdmin(admin.ModelAdmin):
     list_display = ('borrower', 'book', 'borrow_date', 'returned', 'return_date')
     list_editable = ['returned', 'return_date']
 
+class RecommendationAdmin(admin.ModelAdmin):
+    list_display = ('book', 'title', 'timestamp',)
+
 
 admin.site.register(Book, BookAdmin)
 admin.site.register(Borrowing, BorrowingAdmin)
+admin.site.register(Recommendation, RecommendationAdmin)
