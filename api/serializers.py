6b95cf201913e95029a37466b93c3da7a62f8a6d
Abdelmasieh01 <gokuabdo31@gmail.com>
2023-06-26 02:48:01 +0300

    Re-edited the app version

--- api/serializers.py
+++ api/serializers.py
@@ -83,6 +83,6 @@ class AnnouncementSerializer(serializers.ModelSerializer):
 #Serializing the link to the app
 class AppLinkSerializer(serializers.Serializer):
     data = {
-            'version': '1.0.1',
+            'version': '1.0.0',
             'link': 'https://drive.google.com/file/d/1fq5npWfyX9o-tdZSAPsPrpdwAhEXJa1p/view?usp=drive_link',
         }
