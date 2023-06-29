d32145ecd523d2b3c3fe1e7ba1aa438664f90253
Abdelmasieh01 <gokuabdo31@gmail.com>
2023-06-29 11:53:19 +0300

    Reedited the seriliazer i hope all bugs die pls

--- api/serializers.py
+++ api/serializers.py
@@ -29,7 +29,7 @@ class ProfileSerializer(serializers.ModelSerializer):
     photo = serializers.SerializerMethodField()
 
     def get_photo(self, instance):
-        return f'htttps://stpeterlibrary.crabdance.com/media/{instance.photo}' 
+        return f'https://stpeterlibrary.crabdance.com/media/{instance.photo}' 
 
     class Meta:
         model = Profile
